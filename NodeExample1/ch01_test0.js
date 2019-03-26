a6287d8744bba62d64ab6aada1c268fb7048b79a
Master Jung <ajdkfl6445@gmail.com>
2019-03-27 06:37:52 +0900

    2019-03-26 노드 공부2

--- NodeExample1/ch01_test0.js
+++ NodeExample1/ch01_test0.js
@@ -1,1 +1,10 @@
-console.log('안녕하세요!!');
+var result =0;
+
+console.time('duration_sum');
+
+for(var i=1; i<=1000; i++){
+    result += i;
+}
+
+console.timeEnd("duration_sum");
+console.log('1부터 1000까지 더한 결과물 : %d', result);
---
+++ NodeExample1/ch02_test1.js
@@ -0,0 +1,2 @@
+console.log('현재 실행한 파일의 이름 : %s', __filename);
+console.log('현재 실행한 파일의 패스 : %s', __dirname);
